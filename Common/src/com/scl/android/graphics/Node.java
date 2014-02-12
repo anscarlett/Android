356e6d036add5f4c6b072057474de8b80aec4a0c
anscarlett <anscarlett@gmail.com>
2014-02-12 21:23:41 +0000

    Initial commit

---
+++ Common/src/com/scl/android/graphics/Node.java
@@ -0,0 +1,13 @@
+package com.scl.android.graphics;
+
+/**
+ * Created by anscarlett on 12/02/14.
+ */
+public class Node {
+	private static final String CLASS_TAG = Node.class.getCanonicalName();
+	private String mName;
+
+	protected Node() {
+
+	}
+}
